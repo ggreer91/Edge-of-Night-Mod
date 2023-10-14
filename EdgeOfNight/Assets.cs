e869d677b82c0b4f7ad3debb67dc2e29a612ce06
ggreer91 <georgegreer91@gmail.com>
2023-10-14 00:08:24 -0700

    Initialize mod pack

---
+++ EdgeOfNight/Assets.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using BepInEx.Logging;
+using R2API;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace EdgeOfNightMod
+{
+    internal static class Assets
+    {
+        internal static GameObject EdgeOfNightPrefab;
+        internal static Sprite EdgeOfNightIcon;
+
+        internal static ItemDef EdgeOfNightItemDef;
+        internal static ManualLogSource Logger;
+
+        private const string ModPrefix = "@EdgeOfNightMod:";
+
+        internal static void Init(ManualLogSource Logger)
+        {
+            Assets.Logger = Logger;
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("EdgeOfNight.eonassetbundle"))
+            {
+                var bundle = AssetBundle.LoadFromStream(stream);
+
+                EdgeOfNightPrefab = bundle.LoadAsset<GameObject>("Assets/AssetsBundlesWanted/Edge_of_Night.prefab");
+                EdgeOfNightIcon = bundle.LoadAsset<Sprite>("Assets/AssetsBundlesWanted/Edge_of_Night.png");
+            }
+
+            CreateEdgeOfNightItem();
+
+            AddLanguageTokens();
+        }
+
+        private static void CreateEdgeOfNightItem()
+        {
+            EdgeOfNightItemDef = ScriptableObject.CreateInstance<ItemDef>();
+            EdgeOfNightItemDef.name = "EdgeOfNight";
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
+            EdgeOfNightItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/Tier2Def.asset").WaitForCompletion();
+#pragma warning restore Publicizer001
+            EdgeOfNightItemDef.pickupModelPrefab = EdgeOfNightPrefab;
+            EdgeOfNightItemDef.pickupIconSprite = EdgeOfNightIcon;
+            EdgeOfNightItemDef.nameToken = "EDGEOFNIGHT_NAME";
+            EdgeOfNightItemDef.pickupToken = "EDGEOFNIGHT_PICKUP";
+            EdgeOfNightItemDef.descriptionToken = "EDGEOFNIGHT_DESC";
+            EdgeOfNightItemDef.loreToken = "EDGEOFNIGHT_LORE";
+            EdgeOfNightItemDef.tags = [ ItemTag.Utility ];
+
+            var itemDisplayRules = new ItemDisplayRuleDict(null); // make null if we don't want the item on the survivor model
+            //var itemDisplayRules = new ItemDisplayRule[1]; // allows item to show on survivor
+            //itemDisplayRules[0].followerPrefab = EdgeOfNightPrefab; // the prefab that shows up on the survivor
+            //itemDisplayRules[0].childName = "Chest"; // defines starting point of the position of the model, you can see what names are available in the prefab model of the survivors
+            //itemDisplayRules[0].localScale = new Vector3(0.15f, 0.15f, 0.15f); // scale the model
+            //itemDisplayRules[0].localAngles = new Vector3(0f, 0f, 0f); // rotate the model
+            //itemDisplayRules[0].localPos = new Vector3(-0.2f, 1f, -0f); // position offset relative to the childName
+
+            var EdgeOfNight = new R2API.CustomItem(EdgeOfNightItemDef, itemDisplayRules);
+
+            if (!ItemAPI.Add(EdgeOfNight))
+            {
+                EdgeOfNightItemDef = null;
+                Logger.LogError("Unable to add Edge of Night item");
+            }
+        }
+
+        private static void AddLanguageTokens()
+        {
+            // Styles
+            // <style=cIsHealth>" + exampleValue + "</style>
+            // <style=cIsDamage>" + exampleValue + "</style>
+            // <style=cIsHealing>" + exampleValue + "</style>
+            // <style=cIsUtility>" + exampleValue + "</style>
+            // <style=cIsVoid>" + exampleValue + "</style>
+            // <style=cHumanObjective>" + exampleValue + "</style>
+            // <style=cLunarObjective>" + exampleValue + "</style>
+            // <style=cStack>" + exampleValue + "</style>
+            // <style=cWorldEvent>" + exampleValue + "</style>
+            // <style=cArtifact>" + exampleValue + "</style>
+            // <style=cUserSetting>" + exampleValue + "</style>
+            // <style=cDeath>" + exampleValue + "</style>
+            // <style=cSub>" + exampleValue + "</style>
+            // <style=cMono>" + exampleValue + "</style>
+            // <style=cShrine>" + exampleValue + "</style>
+            // <style=cEvent>" + exampleValue + "</style>
+
+            LanguageAPI.Add("EDGEOFNIGHT_NAME", "Edge of Night");
+            LanguageAPI.Add("EDGEOFNIGHT_PICKUP", "Absorbs an instance of an Elite effect.");
+            LanguageAPI.Add("EDGEOFNIGHT_DESC", "Gain <style=cIsUtility>" + 2 + "%</style> crit chance and <style=cIsDamage>" + 3 + "%</style> crit damage"); // currently only showing in logbook
+            LanguageAPI.Add("EDGEOFNIGHT_LORE", "I don't know what this is, but I'll surely find a use for it someday...");
+        }
+    }
+}
---
+++ EdgeOfNight/EdgeOfNight.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using BepInEx;
+using BepInEx.Logging;
+using R2API;
+using R2API.Utils;
+
+using RoR2;
+using UnityEngine;
+
+
+namespace EdgeOfNightMod
+{
+    [BepInDependency(R2API.R2API.PluginGUID)]
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI))]
+    [BepInPlugin(PluginGUID, ModName, ModVer)]
+    public class CustomItem : BaseUnityPlugin
+    {
+        private const string ModVer = "1.1";
+        public const string ModAuthor = "George";
+        public const string ModName = "EdgeofNightMod";
+        public const string PluginGUID = $"{ModAuthor}.{ModName}";
+
+        // internal new static ManualLogSource Log;
+        public static BepInEx.Logging.ManualLogSource Log;
+
+        private static System.Random random = new System.Random();
+        private static bool debounce = false;
+        public void Awake()
+        {
+
+            Log = Logger;
+
+            Assets.Init(Log);
+            On.RoR2.CharacterBody.OnTakeDamageServer += (orig, self, damageReport) => { VerifyBody(orig, self, damageReport); };
+        }
+        public static void VerifyBody(On.RoR2.CharacterBody.orig_OnTakeDamageServer orig, CharacterBody self, DamageReport damageReport)
+        {
+            if (!self.Equals(null) && self.isPlayerControlled)
+            {
+                int edgeOfNight_count = self.inventory.GetItemCount(Assets.EdgeOfNightItemDef);
+                if (edgeOfNight_count > 0)
+                {
+                    DoEdgeOfNight(edgeOfNight_count, self);
+                }
+            }
+
+            orig(self, damageReport);
+        }
+
+        public static void DoEdgeOfNight(int edgeOfNight_count, CharacterBody self)
+        {
+            if (self.healthComponent.isHealthLow)
+            {
+                // we've already executed - skip...
+                if (debounce)
+                {
+                    return;
+                }
+
+                debounce = true;
+                for (int i = 0; i <= edgeOfNight_count; i++)
+                {
+                    var monsters = TeamComponent.GetTeamMembers(TeamIndex.Monster);
+                    if (monsters.Count == 0) // nothing to charm
+                    {
+                        return;
+                    }
+
+                    // copy our monsters list into a randomly sorted order
+                    // it's O(n), but it's the best we can really do here...
+                    var monsters_copy = new List<TeamComponent>(monsters).OrderBy(x => random.Next()).ToList();
+
+                    // grab our random monster that's not a boss
+                    TeamComponent monster = null;
+                    foreach (var m in monsters_copy)
+                    {
+                        if (!m.body.master.isBoss && BossGroup.FindBossGroup(m.body) is null)
+                        {
+                            Log.Log(LogLevel.Debug, "Selected monster: " + m.body.master.name);
+                            monster = m;
+                            break;
+                        }
+                    }
+
+                    // We were unable to find non-boss a monster to charm, lets just exit
+                    // and try again later.
+                    if (monster is null)
+                    {
+                        Log.Log(LogLevel.Warning, "Unable to find a suitable mob to Edge of Night (change this log lol).");
+                        return;
+                    }
+
+                    // Assign to player team
+                    monster.body.master.teamIndex = TeamIndex.Player;
+                    monster.body.teamComponent.teamIndex = TeamIndex.Player;
+
+                    // Reset aggro
+                    var baseAi = monster.body.master.GetComponent<RoR2.CharacterAI.BaseAI>();
+                    baseAi.currentEnemy.Reset();
+                    baseAi.ForceAcquireNearestEnemyIfNoCurrentEnemy();
+                }
+
+                // reset drone aggro if needed
+                var players = TeamComponent.GetTeamMembers(TeamIndex.Player);
+                foreach (var player in players)
+                {
+                    if (!player.body.isPlayerControlled)
+                    {
+                        var ai = player.body.masterObject.GetComponent<RoR2.CharacterAI.BaseAI>();
+                        if (ai.currentEnemy.characterBody.teamComponent.teamIndex == TeamIndex.Player)
+                        {
+                            ai.currentEnemy.Reset();
+                            ai.ForceAcquireNearestEnemyIfNoCurrentEnemy();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                debounce = false;
+            }
+        }
+        private void Update()
+        {
+        //    This if statement checks if the player has currently pressed F2.
+           if (Input.GetKeyDown(KeyCode.F2))
+           {
+               Log.LogInfo("Player pressed F2");
+            //    Get the player body
+               GameObject playerGameObject = PlayerCharacterMasterController.instances[0].master.GetBodyObject();
+               CharacterBody body = playerGameObject.GetComponent<CharacterBody>();
+               if (!body)
+               {
+                   Log.LogInfo("Component body doesn't exist");
+                   return;
+               }
+               if (Util.CheckRoll(50, body.master)) // 50/50 chance
+               {
+                   // if positive roll:
+                   Transform playerTransform = playerGameObject.transform;
+                   Log.LogInfo($"Spawning our custom item at coordinates {playerTransform.position}");
+                   PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(Assets.EdgeOfNightItemDef.itemIndex), playerTransform.position, playerTransform.forward * 20f);
+               }
+               else
+               {
+                   // if negative roll:
+                   DamageInfo info = new DamageInfo();
+                   info.damage = (body.maxHealth / 10);
+                   info.crit = false;
+                   info.procCoefficient = 0f;
+                   body.healthComponent.TakeDamage(info);
+                   Log.LogInfo($"Player rolled negatively - took {info.damage} damage");
+               }
+           }
+        }
+    }
+}
