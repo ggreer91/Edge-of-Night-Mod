9e6da42aff38646d2cbe757989d40afe9fd8c3fd
ggreer91 <georgegreer91@gmail.com>
2023-10-24 21:37:25 -0700

    Disable old item effect in preparation for implementation of the new one

--- EdgeOfNight/EdgeOfNight.cs
+++ EdgeOfNight/EdgeOfNight.cs
@@ -34,102 +34,109 @@ namespace EdgeOfNightMod
             Log = Logger;
 
             Assets.Init(Log);
-            On.RoR2.CharacterBody.OnTakeDamageServer += (orig, self, damageReport) => { VerifyBody(orig, self, damageReport); };
+            /*
+             * 
+             * ------------------------------ EVERYTHING BELOW THIS COMMENT THAT IS COMMENTED IS FOR A CHARMING EFFECT ------------------------------
+             * 
+             */
+            //On.RoR2.CharacterBody.OnTakeDamageServer += (orig, self, damageReport) => { VerifyBody(orig, self, damageReport); };
         }
-        public static void VerifyBody(On.RoR2.CharacterBody.orig_OnTakeDamageServer orig, CharacterBody self, DamageReport damageReport)
-        {
-            if (!self.Equals(null) && self.isPlayerControlled)
-            {
-                int edgeOfNight_count = self.inventory.GetItemCount(Assets.EdgeOfNightItemDef);
-                if (edgeOfNight_count > 0)
-                {
-                    DoEdgeOfNight(edgeOfNight_count, self);
-                }
-            }
+        //public static void VerifyBody(On.RoR2.CharacterBody.orig_OnTakeDamageServer orig, CharacterBody self, DamageReport damageReport)
+        //{
+        //    if (!self.Equals(null) && self.isPlayerControlled)
+        //    {
+        //        int edgeOfNight_count = self.inventory.GetItemCount(Assets.EdgeOfNightItemDef);
+        //        if (edgeOfNight_count > 0)
+        //        {
+        //            DoEdgeOfNight(edgeOfNight_count, self);
+        //        }
+        //    }
 
-            orig(self, damageReport);
-        }
+        //    orig(self, damageReport);
+        //}
 
-        public static void DoEdgeOfNight(int edgeOfNight_count, CharacterBody self)
-        {
-            if (self.healthComponent.isHealthLow)
-            {
-                // we've already executed - skip...
-                if (debounce)
-                {
-                    return;
-                }
+        //public static void DoEdgeOfNight(int edgeOfNight_count, CharacterBody self)
+        //{
+        //    if (self.healthComponent.isHealthLow)
+        //    {
+        //        // we've already executed - skip...
+        //        if (debounce)
+        //        {
+        //            return;
+        //        }
 
-                debounce = true;
-                for (int i = 0; i <= edgeOfNight_count; i++)
-                {
-                    var monsters = TeamComponent.GetTeamMembers(TeamIndex.Monster);
-                    if (monsters.Count == 0) // nothing to charm
-                    {
-                        return;
-                    }
+        //        debounce = true;
+        //        for (int i = 0; i <= edgeOfNight_count; i++)
+        //        {
+        //            var monsters = TeamComponent.GetTeamMembers(TeamIndex.Monster);
+        //            if (monsters.Count == 0) // nothing to charm
+        //            {
+        //                return;
+        //            }
 
-                    // copy our monsters list into a randomly sorted order
-                    // it's O(n), but it's the best we can really do here...
-                    var monsters_copy = new List<TeamComponent>(monsters).OrderBy(x => random.Next()).ToList();
+        //            // copy our monsters list into a randomly sorted order
+        //            // it's O(n), but it's the best we can really do here...
+        //            var monsters_copy = new List<TeamComponent>(monsters).OrderBy(x => random.Next()).ToList();
 
-                    // grab our random monster that's not a boss
-                    TeamComponent monster = null;
-                    foreach (var m in monsters_copy)
-                    {
-                        if (!m.body.master.isBoss && BossGroup.FindBossGroup(m.body) is null)
-                        {
-                            Log.Log(LogLevel.Debug, "Selected monster: " + m.body.master.name);
-                            monster = m;
-                            break;
-                        }
-                    }
+        //            // grab our random monster that's not a boss
+        //            TeamComponent monster = null;
+        //            foreach (var m in monsters_copy)
+        //            {
+        //                if (!m.body.master.isBoss && BossGroup.FindBossGroup(m.body) is null)
+        //                {
+        //                    Log.Log(LogLevel.Debug, "Selected monster: " + m.body.master.name);
+        //                    monster = m;
+        //                    break;
+        //                }
+        //            }
 
-                    // We were unable to find non-boss a monster to charm, lets just exit
-                    // and try again later.
-                    if (monster is null)
-                    {
-                        Log.Log(LogLevel.Warning, "Unable to find a suitable mob to Edge of Night (change this log lol).");
-                        return;
-                    }
+        //            // We were unable to find non-boss a monster to charm, lets just exit
+        //            // and try again later.
+        //            if (monster is null)
+        //            {
+        //                Log.Log(LogLevel.Warning, "Unable to find a suitable mob to Edge of Night (change this log lol).");
+        //                return;
+        //            }
 
-                    // Assign to player team
-                    monster.body.master.teamIndex = TeamIndex.Player;
-                    monster.body.teamComponent.teamIndex = TeamIndex.Player;
+        //            // Assign to player team
+        //            monster.body.master.teamIndex = TeamIndex.Player;
+        //            monster.body.teamComponent.teamIndex = TeamIndex.Player;
 
-                    // Reset aggro
-                    var baseAi = monster.body.master.GetComponent<RoR2.CharacterAI.BaseAI>();
-                    baseAi.currentEnemy.Reset();
-                    baseAi.ForceAcquireNearestEnemyIfNoCurrentEnemy();
-                }
+        //            // Reset aggro
+        //            var baseAi = monster.body.master.GetComponent<RoR2.CharacterAI.BaseAI>();
+        //            baseAi.currentEnemy.Reset();
+        //            baseAi.ForceAcquireNearestEnemyIfNoCurrentEnemy();
+        //        }
 
-                // reset drone aggro if needed
-                var players = TeamComponent.GetTeamMembers(TeamIndex.Player);
-                foreach (var player in players)
-                {
-                    if (!player.body.isPlayerControlled)
-                    {
-                        var ai = player.body.masterObject.GetComponent<RoR2.CharacterAI.BaseAI>();
-                        if (ai.currentEnemy.characterBody.teamComponent.teamIndex == TeamIndex.Player)
-                        {
-                            ai.currentEnemy.Reset();
-                            ai.ForceAcquireNearestEnemyIfNoCurrentEnemy();
-                        }
-                    }
-                }
-            }
-            else
-            {
-                debounce = false;
-            }
-        }
+        //        // reset drone aggro if needed
+        //        var players = TeamComponent.GetTeamMembers(TeamIndex.Player);
+        //        foreach (var player in players)
+        //        {
+        //            if (!player.body.isPlayerControlled)
+        //            {
+        //                var ai = player.body.masterObject.GetComponent<RoR2.CharacterAI.BaseAI>();
+        //                if (ai.currentEnemy.characterBody.teamComponent.teamIndex == TeamIndex.Player)
+        //                {
+        //                    ai.currentEnemy.Reset();
+        //                    ai.ForceAcquireNearestEnemyIfNoCurrentEnemy();
+        //                }
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        debounce = false;
+        //    }
+        //}
+
+        // Runs on every frame
         private void Update()
         {
-        //    This if statement checks if the player has currently pressed F2.
+        //    Checking if player presses F2
            if (Input.GetKeyDown(KeyCode.F2))
            {
                Log.LogInfo("Player pressed F2");
-            //    Get the player body
+            //    Gets player body
                GameObject playerGameObject = PlayerCharacterMasterController.instances[0].master.GetBodyObject();
                CharacterBody body = playerGameObject.GetComponent<CharacterBody>();
                if (!body)
